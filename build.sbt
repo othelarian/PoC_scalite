6033d6dad9c47d5932c94a7cfb6416ddaac03a0b
othelarian <le.maitre.killian@gmail.com>
2020-05-01 01:58:52 +0200

    first steps

--- build.sbt
+++ build.sbt
@@ -4,4 +4,10 @@ name := "PoCScalite"
 organization := "play.othy.scala"
 version := "1.0"
 
-libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
+//libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
+libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.23.1"
+libraryDependencies ++= Seq(
+  "org.tpolecat" %% "doobie-core" % "0.8.8",
+  "org.tpolecat" %% "doobie-hikari" % "0.8.8",
+  "org.tpolecat" %% "doobie-specs2" % "0.8.8"
+)
--- src/main/scala/PoCScalite.scala
+++ src/main/scala/PoCScalite.scala
@@ -1,10 +1,26 @@
 
-import cats._
-import cats.implicits._
+//import doobie._
+import doobie.implicits._
+import doobie.util.transactor.Transactor
+//import cats._
+//import cats.data._
+import cats.effect.IO
+import scala.concurrent.ExecutionContext
+//import cats.implicits._
 
 object PoCScalite extends App {
 
-    //
-    //
+    implicit val cs = IO.contextShift(ExecutionContext.global)
 
+    val prog = sql"select 42".query[Int].unique
+
+    val transactor = Transactor.fromDriverManager[IO](
+        "org.sqlite.JDBC", "jdbc:sqlite:sample.db", "", ""
+    )
+    //
+    //
+    //
+    val io = prog.transact(transactor)
+    val r = io.unsafeRunSync
+    println(r)
 }
