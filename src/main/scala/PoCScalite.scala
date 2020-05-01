0ceccf8daff74765fc67963a96a94caef305182a
othelarian <le.maitre.killian@gmail.com>
2020-05-02 01:46:34 +0200

    get error handle, and some selection

--- src/main/scala/PoCScalite.scala
+++ src/main/scala/PoCScalite.scala
@@ -1,26 +1,33 @@
 
 //import doobie._
+import doobie.ConnectionIO
 import doobie.implicits._
 import doobie.util.transactor.Transactor
-//import cats._
+import cats._
 //import cats.data._
 import cats.effect.IO
+import cats.implicits._
 import scala.concurrent.ExecutionContext
-//import cats.implicits._
 
 object PoCScalite extends App {
 
     implicit val cs = IO.contextShift(ExecutionContext.global)
 
-    val prog = sql"select 42".query[Int].unique
+    //val prog = sql"select * from sqlite_master".query[Int].unique
 
     val transactor = Transactor.fromDriverManager[IO](
         "org.sqlite.JDBC", "jdbc:sqlite:sample.db", "", ""
     )
+    def safeCall: ConnectionIO[Either[Throwable, List[String]]] = {
+    //def safeCall: ConnectionIO[Either[Throwable, String]] = {
+        sql"select type, name from sqlite_master".query[String].to[List]
+            .attempt
+    }
     //
     //
     //
-    val io = prog.transact(transactor)
-    val r = io.unsafeRunSync
+    val r = safeCall.transact(transactor).unsafeRunSync
+    //val io = prog.transact(transactor)
+    //val r = io.unsafeRunSync
     println(r)
 }
