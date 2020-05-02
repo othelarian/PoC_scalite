4b160d36756c3e722cee2b05f19f90be19ac04ea
othelarian <le.maitre.killian@gmail.com>
2020-05-02 18:40:38 +0200

    chaining test

--- src/main/scala/PoCScalite.scala
+++ src/main/scala/PoCScalite.scala
@@ -11,23 +11,52 @@ import scala.concurrent.ExecutionContext
 
 object PoCScalite extends App {
 
-    implicit val cs = IO.contextShift(ExecutionContext.global)
+  implicit val cs = IO.contextShift(ExecutionContext.global)
 
-    //val prog = sql"select * from sqlite_master".query[Int].unique
+  // requests
+  val list_tables = sql"SELECT type, name FROM sqlite_master".query[(String, String)].to[List]
+  val create_tt1 = sql"CREATE TABLE tt1 (idx INTEGER)".update.run
+  val drop_tt1 = sql"DROP TABLE IF EXISTS tt1".update.run
 
-    val transactor = Transactor.fromDriverManager[IO](
-        "org.sqlite.JDBC", "jdbc:sqlite:sample.db", "", ""
-    )
-    def safeCall: ConnectionIO[Either[Throwable, List[String]]] = {
-    //def safeCall: ConnectionIO[Either[Throwable, String]] = {
-        sql"select type, name from sqlite_master".query[String].to[List]
-            .attempt
-    }
-    //
-    //
-    //
-    val r = safeCall.transact(transactor).unsafeRunSync
-    //val io = prog.transact(transactor)
-    //val r = io.unsafeRunSync
-    println(r)
+  val transactor = Transactor.fromDriverManager[IO](
+    "org.sqlite.JDBC", "jdbc:sqlite:sample.db", "", ""
+  )
+
+  def safeCall: ConnectionIO[Either[Throwable, List[(String, String)]]] = {
+    sql"select type, name from sqlite_master".query[(String, String)].to[List]
+      .attempt
+  }
+  def callprint(tup: (String, String)) = println(s"type: ${tup._1}, name: ${tup._2}")
+  /*
+  safeCall.transact(transactor).unsafeRunSync match {
+    case Right(v) =>
+      v map callprint
+      val r = v contains ("table", "tt1")
+    case Left(e) => ()
+  }
+  */
+  //
+  //def safeCall2 = (drop_tt1.attempt, create_tt1.attempt, list_tables.attempt)
+  //def safeCall2 = (drop_tt1, create_tt1).flatMap
+  //
+  def t1 = create_tt1
+  def t2 = drop_tt1
+  def t3 = t1.flatMap(_ => t2).attempt
+  //
+  def t4 =
+    for {
+      a <- t1
+      b <- t2
+    } yield b
+  //
+  val r = t3.transact(transactor).unsafeRunSync /*match {
+    case Left(e) => ()
+    case Right(v) =>
+      //
+      println(v)
+      //
+  }*/
+  //
+  println(r)
+  //
 }
